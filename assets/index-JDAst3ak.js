3f26d2d8ba260e98cdc6204255ee150ad6e411da
Gaius-98 <2388838628@qq.com>
2024-12-10 02:35:01 +0000

    fix(table-design): adjust table design layout

--- assets/index-JDAst3ak.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./.pnpm-DpKfdl5h.js";const a=e({__name:"index",setup(t){return(n,r)=>"table"}});export{a as default};
--- assets/index-JPyfuYoy.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./.pnpm-DpKfdl5h.js";const o=e({__name:"index",setup(n){return(t,r)=>null}});export{o as default};
