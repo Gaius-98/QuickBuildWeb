26c0463721c51d657b83406d02bbfe1b35b83fd5
Gaius-98 <2388838628@qq.com>
2024-12-29 13:57:12 +0000

    feat(dynamic-config): add DynamicConfig class and IDynamicConfig model for dynamic value handling

---
+++ assets/CodeEditor-IP_-A7xe.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./CodeEditor.vue_vue_type_script_setup_true_name_EvCode_lang-fTuPaE3B.js";import"./.pnpm-B6SttuKA.js";export{m as default};
---
+++ assets/index-CBe293MX.js
@@ -0,0 +1,1 @@
+import{g as e}from"./.pnpm-B6SttuKA.js";const o=e({__name:"index",setup(n){return(t,r)=>null}});export{o as default};
