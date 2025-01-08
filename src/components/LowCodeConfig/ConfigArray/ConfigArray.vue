4e55b11f82b4b0c6c7fefb027b364a4d9ef3911b
wangguoshuai <2388838628@qq.com>
2025-01-08 16:41:32 +0800

    feat(ConfigArray): refactor data handling and remove unused properties for improved clarity

--- src/components/LowCodeConfig/ConfigArray/ConfigArray.vue
+++ src/components/LowCodeConfig/ConfigArray/ConfigArray.vue
@@ -5,99 +5,57 @@
       type="editable-card"
       @edit="onEdit"
       class="config-array-tab"
-      v-if="!isSelected"
     >
       <a-tab-pane
-        v-for="(pane, idx) in data._value"
+        v-for="(pane, idx) in data"
         :key="idx + 1"
         :tab="`${title}${idx + 1}`"
         :closable="closable"
       >
-        #TODO 表单渲染组件
+        <config-form :schema="schema" v-model:data="data[idx]"> </config-form>
       </a-tab-pane>
-      <template #rightExtra>
-        <a-button
-          :title="isSelected ? `绑定中:${data._dynExp}` : '变量绑定'"
-          :type="isSelected ? 'primary' : ''"
-          @click="onToggleMode()"
-        >
-          <template #icon>
-            <div class="iconfont icon-brackets-curly"></div>
-          </template>
-        </a-button>
-      </template>
     </a-tabs>
-    <a-input
-      v-if="isSelected"
-      v-model:value="data._dynExp"
-      style="width: calc(100% - 35px)"
-      prefix="{"
-      suffix="}"
-    >
-    </a-input>
-    <a-button
-      v-if="isSelected"
-      :title="isSelected ? `绑定中:${data._dynExp}` : '变量绑定'"
-      :type="isSelected ? 'primary' : ''"
-      @click="onToggleMode()"
-    >
-      <template #icon>
-        <div class="iconfont icon-brackets-curly"></div>
-      </template>
-    </a-button>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { reactive, toRefs, ref, computed } from 'vue'
-interface DataProps {
-  _value: Record<string, any>[]
-  _mode: 'static' | 'dynamic'
-  _dynExp: string
-}
+import type { ConfigSchema } from '@/model'
+import ConfigForm from '../ConfigForm/ConfigForm.vue'
+
 interface Props {
   title: string
   closable: boolean
+  schema: ConfigSchema
+  data: Record<string, any>[]
 }
 
 const props = withDefaults(defineProps<Props>(), {
   title: '标题',
-  closable: true
+  closable: true,
+  schema: () => ({})
 })
-const { title, closable } = toRefs(props)
+const { title, closable, data } = toRefs(props)
 const activeKey = ref(0)
-const data = defineModel<DataProps>('data', {
-  required: true,
-  default: {
-    _value: [],
-    _mode: 'static',
-    _dynExp: ''
-  }
-})
-const isSelected = computed(() => {
-  return data.value._mode === 'dynamic'
-})
-const onToggleMode = () => {
-  data.value._mode = data.value._mode === 'dynamic' ? 'static' : 'dynamic'
-}
+
 const add = () => {
-  activeKey.value = data.value._value.length || 0
-  data.value._value.push({})
+  activeKey.value = data.value.length || 0
+  data.value.push({})
 }
 
 const remove = (targetKey: number) => {
   let lastIndex = 0
-  data.value._value.forEach((pane, i) => {
+  data.value.forEach((pane, i) => {
     if (pane.key === targetKey) {
       lastIndex = i - 1
     }
   })
-  data.value._value = data.value._value.filter((pane) => pane.key !== targetKey)
-  if (data.value._value.length && activeKey.value === targetKey) {
+  data.value = data.value.filter((pane) => pane.key !== targetKey)
+  if (data.value.length && activeKey.value === targetKey) {
     if (lastIndex >= 0) {
-      activeKey.value = data.value._value[lastIndex].key
+      activeKey.value = data.value[lastIndex].key
     } else {
-      activeKey.value = data.value._value[0].key
+      activeKey.value = data.value[0].key
     }
   }
 }
