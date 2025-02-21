4262601f350cdffa6deecc54f969b273434b387a
Gaius-98 <2388838628@qq.com>
2025-02-21 03:01:24 +0000

    fix(vite.config): remove surplus config

---
+++ assets/CodeEditor-CGj12C1T.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CodeEditor.vue_vue_type_script_setup_true_name_EvCode_lang-Dd6wsA-g.js";import"./iconfont-DlnTACa0.js";import"./dark-CYikDsl4.js";export{o as default};
---
+++ assets/index-BAbflK5K.js
@@ -0,0 +1,1 @@
+import{C as n,A as s}from"./Col-7N7sU2gK.js";import{S as o}from"./iconfont-DlnTACa0.js";const a=o(n),p=o(s);export{a as _,p as a};
