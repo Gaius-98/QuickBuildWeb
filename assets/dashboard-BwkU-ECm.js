bed035f05fb50609f4af9c1d225c7012e555b369
Gaius-98 <2388838628@qq.com>
2025-05-15 06:13:42 +0000

    fix(dashboard): fix dashboard preview

---
+++ assets/dashboard-BwkU-ECm.js
@@ -0,0 +1,1 @@
+import{r as t}from"./main-Ch5DcOKu.js";const d={getList:r=>t({method:"get",url:"dashboard/list",params:r}),getDetail:r=>t({method:"get",url:"dashboard/detail",params:{id:r}}),remove:r=>t({method:"get",url:"dashboard/remove",params:{id:r}}),add:r=>t({method:"post",url:"dashboard/add",data:r}),update:r=>t({method:"post",url:"dashboard/update",data:r})};export{d};
